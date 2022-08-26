cae0e71b685b639c283ab57bbe07ee4c82753d5f
Adam <adam.salvius@gmail.com>
2022-08-26 19:29:46 +0200

    html

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // eslint-disable-next-line no-unused-vars
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
